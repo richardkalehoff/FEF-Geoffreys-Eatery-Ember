fd9e5a1a423ba906b547a87fd9072da268a91a66
Richard Kalehoff <richardkalehoff@gmail.com>
2015-10-28 15:37:54 -0700

    move data out of template and into model() hook

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [{
+      name: 'stovetop-lasagna',
+      title: 'Stovetop Lasagna',
+      rating: 3.1,
+      img: 'lasagna'
+    }, {
+      name: 'shake-n-shimmy-pork-chops',
+      title: 'Shake n\' Shimmy Pork Chops',
+      rating: 3.7,
+      img: 'pork-chops'
+    }, {
+      name: 'caramel-chocolate-and-candy-apples',
+      title: 'Caramel, Chocolate and Candy Apples',
+      rating: 5,
+      img: 'candy-apples'
+    }, {
+      name: 'deviled-chicken-lollipops',
+      title: 'Deviled Chicken Lollipops',
+      rating: 4.2,
+      img: 'chicken-lollipops'
+    }];
+  }
 });
